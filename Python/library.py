382f0928991aed01ec77848b847062ade81fbbc6
Glassandra <153222628+Glassandra@users.noreply.github.com>
2024-09-18 11:14:33 +0200

    Added summary and changed some structure of borrow_item Added working status for Item

--- Python/library.py
+++ Python/library.py
@@ -4,16 +4,13 @@ class Item:
         self._title = title
         self._author = author
         self._year = year
-        self._status = "" #todo
+        self._status = ""
 
     def display_info(self):
         pass
 
     def set_status(self, status):
         self._status = status
-
-    def get_status(self):
-        return self._status
 
 class Book(Item):
 
@@ -49,7 +46,6 @@ class Library:
     def __init__(self):
         self._items = []
         self._users = []
-        self._borrowedItems = []
 
     def add_item(self, Item):
         self._items.append(Item)
@@ -62,28 +58,35 @@ class Library:
 
     def borrow_item(self, LibraryUser, Item):
         if Item in self._items:
-            self._borrowedItems.append(Item)
-            LibraryUser.borrow(Item)
-            self._items.remove(Item)
+            if Item._status == "":
+                Item.set_status(LibraryUser._name)
+                LibraryUser.borrow(Item)
+            else:
+                print(f"Not possible, {Item._title} is borrowed by {Item._status}") 
+        else:
+            print(f"Not possible, {Item._title} does not exist.")
 
     def return_item(self, LibraryUser, Item):
-        self._items.append(Item)
+        Item.set_status("")
         LibraryUser.return_item(Item)
-        self._borrowedItems.remove(Item)
     
-    def list_items(self):
-        self._list_available_items
-        for item in self.borrow_item:
-            item.display_info()
-            print("---------")
-
-    def list_available_items(self):        
+    def list_items(self):        
         for item in self._items:
             item.display_info()            
             print("---------")
 
+    def list_available_items(self):
+        for item in self._items:
+            if item._status == "":
+                item.display_info()            
+                print("---------")
+
     def summary(self):
-        print("Todo")
+        for item in self._items:
+            if item._status != "":
+                print(f"{item._title} is borrowed by {item._status}")
+            else:
+                print(f"{item._title} is available")
 
 
 library = Library()
@@ -95,6 +98,7 @@ library.register_user(user2)
 
 book1 = Magazine("News", "Pelle", 2005, "Street")
 book2 = Book("Swan Lake", "Emma", 1999, 500)
+book3 = Book("404", "null", 000, 0)
 
 library.add_item(book1)
 library.add_item(book2)
@@ -110,4 +114,11 @@ library.add_item(book1)
 
 library.borrow_item(user1, book2)
 print("---------")
+print("Available items")
 library.list_available_items()
+print("All items")
+library.list_items()
+
+library.borrow_item(user2, book2)
+library.borrow_item(user2, book3)
+library.summary()
