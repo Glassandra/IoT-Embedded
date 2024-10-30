299ecac5050790afc7d2ae0cfc7d6608b9129d58
Glassandra <153222628+Glassandra@users.noreply.github.com>
2024-10-30 09:12:10 +0100

    Added version bitwise operations

--- C/v43/binary_numbers_binary_operators.cpp
+++ C/v43/binary_numbers_binary_operators.cpp
@@ -2,7 +2,7 @@ byte ledPins[] = {16, 17, 18, 19, 20, 21, 22, 26, 27, 28};
 int ledCounts;
 
 void setup() {
-    ledCounts = sizeof(ledPins) / sizeof(ledPins[0]); // Correct calculation of ledCounts
+    ledCounts = sizeof(ledPins) / sizeof(ledPins[0]);
     for (byte pin : ledPins) {
         pinMode(pin, OUTPUT);
     }
@@ -10,7 +10,6 @@ void setup() {
 
 void setLEDs(int number) {
     for (int i = 0; i < ledCounts; i++) {
-        // Use bitwise AND and shift to determine the state of each bit
         digitalWrite(ledPins[i], (number & (1 << i)) ? HIGH : LOW);
     }
 }
